7dbc26d793ceb408c235537bd4ec414f5a92e3eb
DimBox <dimbox@inbox.ru>
2019-04-11 22:30:27 +0700

    Modified application ajax_colorer

--- ajax_colorer/urls.py
+++ ajax_colorer/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'home', views.home_page),
+    url(r'change', views.change_color),
     url(r'$', views.home_page),
 ]
--- ajax_colorer/views.py
+++ ajax_colorer/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.template import loader
+from django.http import JsonResponse
 from colorer import compl
-import jsonpickle
 import random
 
 color_default = [{'bg': '#563d7c', 'txt': 'rgba(255, 255, 255, 0.5)', 'txth': 'rgba(255, 255, 255, 0.7)',
@@ -14,14 +13,12 @@ context = {
     'title': 'Калейдоскоп-AJAX',
     'value': [article],
     'colors': color_default,
-    'json_colors': jsonpickle.encode(color_default),
 }
 # Create your views here.
 def home_page(request):
-    color_history = [{'bg': '#563d7c', 'txt': 'rgba(255, 255, 255, 0.5)', 'txth': 'rgba(255, 255, 255, 0.7)',
+    color_default = [{'bg': '#563d7c', 'txt': 'rgba(255, 255, 255, 0.5)', 'txth': 'rgba(255, 255, 255, 0.7)',
                       'texb': '#FFFFFF'}]
-    context['colors'] = color_history
-    context['json_colors'] = jsonpickle.encode(color_history)
+    context['colors'] = color_default
     return render(request, template_name='ajax_colorer/base.html', context=context)
 
 
@@ -33,5 +30,14 @@ def random_color(prefix, maxlen):
 def detect_text_color(color):
     return compl.contrast(color)
 
+
 def change_color(request):
-    pass
+    new_color = random_color('#', 6)
+    text_color = detect_text_color(new_color)
+    if text_color in ['#FFFFFF', '#ffffff']:
+        btn_color = '#000000'
+    else:
+        btn_color = '#FFFFFF'
+    context = {'bg': new_color, 'txt': compl.hex_to_rgba(text_color, 0.5),
+            'txth': compl.hex_to_rgba(text_color, 0.7), 'txtb': btn_color}
+    return JsonResponse(context)
